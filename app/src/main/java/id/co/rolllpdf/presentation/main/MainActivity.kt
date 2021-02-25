31d78534f5a95f80ae213c34a2cd94a0f78af5b8
Irfan Pertadima <pertadimairfan@gmail.com>
2021-02-25 15:07:30 +0700

    [irfan] feat: handle request permissiona ndroid

--- app/src/main/java/id/co/rolllpdf/presentation/main/MainActivity.kt
+++ app/src/main/java/id/co/rolllpdf/presentation/main/MainActivity.kt
@@ -1,12 +1,17 @@
 package id.co.rolllpdf.presentation.main
 
+import android.Manifest
 import id.co.rolllpdf.base.BaseActivity
 import id.co.rolllpdf.databinding.ActivityMainBinding
 import id.co.rolllpdf.presentation.customview.DialogProFeatureView
 import id.co.rolllpdf.util.NestedScrollViewOverScrollDecorAdapter
 import me.everything.android.ui.overscroll.VerticalOverScrollBounceEffectDecorator
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), EasyPermissions.PermissionCallbacks,
+    EasyPermissions.RationaleCallbacks {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -21,6 +26,7 @@ class MainActivity : BaseActivity() {
         changeStatusBarColor(android.R.color.white)
         initOverScroll()
         showProView()
+        floatingActionButtonListener()
     }
 
     override fun onViewModelObserver() {
@@ -48,5 +54,88 @@ class MainActivity : BaseActivity() {
                 }
             }
         }
+    }
+
+    private fun floatingActionButtonListener() {
+        binding.mainFabAdd.setOnClickListener {
+            checkStoragePermission {
+
+            }
+        }
+    }
+
+    @AfterPermissionGranted(Permission.MEDIA)
+    private fun checkStoragePermission(onHasPermission: () -> Unit) {
+        val perms = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            arrayOf(
+                Manifest.permission.ACCESS_MEDIA_LOCATION,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+        }
+
+        if (EasyPermissions.hasPermissions(this, *perms)) {
+            onHasPermission.invoke()
+        } else {
+            // Do not have permissions, request them now
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                EasyPermissions.requestPermissions(
+                    this,
+                    "",
+                    Permission.MEDIA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.ACCESS_MEDIA_LOCATION,
+                )
+            } else {
+                EasyPermissions.requestPermissions(
+                    this,
+                    "",
+                    Permission.MEDIA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //TODO: GO TO CAMERA SCREEN
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+
+    }
+
+    object Permission {
+        const val MEDIA = 101
     }
 }
