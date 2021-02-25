2e26f6eb9c88d3bf4d3365ea55cb0ae37a6f8b76
Irfan Pertadima <pertadimairfan@gmail.com>
2021-02-25 13:09:00 +0700

    [irfan] feat: create poligon view

---
+++ libs/core/src/main/java/id/co/rolllpdf/core/DataTypeExtentions.kt
@@ -0,0 +1,9 @@
+package id.co.rolllpdf.core
+
+/*
+ * Created by pertadima on 25,February,2021
+ */
+
+fun Float?.orZero() = this ?: 0F
+fun Int?.orZero() = this ?: 0
+fun Double?.orZero() = this ?: 0.0
